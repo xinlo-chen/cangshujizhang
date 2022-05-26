78c68173f5f4afbd8fc702b09fc65807e98f1f13
xinlo <1311257306@qq.com>
2022-05-26 16:13:10 +0800

    数据保存至localStorage

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,7 +1,7 @@
 <template>
   <layout class-prefix="layout">
-    {{ record }}
-    <NumberPad @update:value="onUpdateAmount"/>
+    {{ recordList }}
+    <NumberPad @update:value="onUpdateAmount" @submit="saveRecord"/>
     <Types :value.sync="record.type"/>
     <Notes @update:value="onUpdateNotes"/>
     <Tags :data-source.sync="tags" @update:value="onUpdateTags"/>
@@ -14,13 +14,26 @@ import NumberPad from '@/components/Money/NumberPad.vue';
 import Types from '@/components/Money/Types.vue';
 import Notes from '@/components/Money/Notes.vue';
 import Tags from '@/components/Money/Tags.vue';
-import {Component} from 'vue-property-decorator';
+import {Component, Watch} from 'vue-property-decorator';
+
+// const version = window.localStorage.getItem('version') || '0';
+// const recordList: Record[] = JSON.parse(window.localStorage.getItem('recordList') || '[]');
+// if (version === '1.0.0') {
+//   //数据库升级，数据迁移
+//   recordList.forEach(record => {
+//     record.createdAt = new Date(2020, 0, 1);
+//   });
+//   //保存数组
+//   window.localStorage.setItem('recordList', JSON.stringify(recordList));
+// }
+// window.localStorage.setItem('version', '1.0.1');
 
 type Record = {
   tags: string[]
   notes: string
   type: string
-  amount: number
+  amount: number //数据类型
+  createdAt?: Date   //类/构造函数
 }
 
 @Component({
@@ -28,6 +41,7 @@ type Record = {
 })
 export default class Money extends Vue {
   tags = ['衣', '食', '住', '行'];
+  recordList: Record[] = JSON.parse(window.localStorage.getItem('recordList') || '[]');
   record: Record = {tags: [], notes: '', type: '-', amount: 0};
 
   onUpdateTags(value: string[]) {
@@ -37,6 +51,17 @@ export default class Money extends Vue {
   onUpdateNotes(value: string) {
     this.record.notes = value;
   };
+
+  saveRecord() {
+    const record2: Record = JSON.parse(JSON.stringify(this.record));
+    record2.createdAt = new Date();
+    this.recordList.push(record2);
+  }
+
+  @Watch('recordList')
+  onRecordListChange() {
+    window.localStorage.setItem('recordList', JSON.stringify(this.recordList));
+  }
 
   // onUpdateTypes(value: string) {
   //   this.record.type = value;
