5e4e36f5352733b5cb136064b79de01b50637bd6
xinlo <1311257306@qq.com>
2022-06-02 16:40:51 +0800

    将不同模块的store进行分拆

--- src/store/index2.ts
+++ src/store/index2.ts
@@ -1,35 +1,9 @@
-import recordListModel from '@/models/recordListModel';
-import tagsListModel from '@/models/tagsListModel';
+import recordStore from '@/store/recordStore';
+import tagStore from '@/store/tagStore';
 
 const store = {
-  //record store
-  recordList: recordListModel.fetch(),
-
-  createRecord: (record: RecordItem) => recordListModel.create(record),
-
-  //Tag store
-  tagList: tagsListModel.fetch(),
-
-  findTag(id: string) {
-    return this.tagList.filter(t => t.id === id)[0];
-  },
-
-  createTag: (name: string) => {
-    const message = tagsListModel.create(name);  //知识点2 ，最小知识原则
-    if (message === 'duplicated') {
-      window.alert('标签重复');
-    } else if (message === 'success') {
-      window.alert('添加成功');
-    }
-  },
-
-  removeTag: (id: string) => {
-    return tagsListModel.remove(id);
-  },
-
-  updateTag: (id: string, name: string) => {
-    return tagsListModel.update(id, name);
-  }
+  ...recordStore,
+  ...tagStore
 };
 
 export default store;
