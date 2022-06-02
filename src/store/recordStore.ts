1b24cdbf6f816ec3c28b91ee9b787591db6c9256
xinlo <1311257306@qq.com>
2022-06-02 17:46:21 +0800

    模块化的store ，删除了models

--- src/store/recordStore.ts
+++ src/store/recordStore.ts
@@ -1,7 +1,26 @@
-import recordListModel from '@/models/recordListModel';
+import clone from '@/lib/clone';
 
-export default {
+const localstorageKeyName = 'recordList';
+let data:RecordItem[] | undefined = undefined;
+
+const recordStore = {
   //record store
-  recordList: recordListModel.fetch(),
-  createRecord: (record: RecordItem) => recordListModel.create(record),
+  recordList: [] as RecordItem[],
+  fetchRecords() {
+    this.recordList = JSON.parse(window.localStorage.getItem(localstorageKeyName) || '[]') as RecordItem[];
+    return this.recordList;
+  },
+  saveRecords() {
+    window.localStorage.setItem(localstorageKeyName, JSON.stringify(this.recordList));
+  },
+  createRecord(record: RecordItem){
+    const record2: RecordItem = clone(record);
+    record2.createdAt = new Date();
+    this.recordList && this.recordList.push(record2);
+    recordStore.saveRecords();
+  },
 };
+
+recordStore.fetchRecords();
+
+export default recordStore;
