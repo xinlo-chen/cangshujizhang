16c22190d10f826961e3b78229df65e43fbbd68a
xinlo <1311257306@qq.com>
2022-06-07 16:07:48 +0800

    Money.vue使用vuex

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,15 +1,27 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from 'vue';
+import Vuex from 'vuex';
+import clone from '@/lib/clone';
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
-export default new Vuex.Store({
-  state: {
+const store = new Vuex.Store({
+  state: {  //data
+    recordList: [] as RecordItem[]
   },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
+  mutations: {  //methods
+    fetchRecords(state) {
+      state.recordList = JSON.parse(window.localStorage.getItem('recordList') || '[]') as RecordItem[];
+    },
+    createRecord(state,record) {
+      const record2: RecordItem = clone(record);
+      record2.createdAt = new Date();
+      state.recordList.push(record2);
+      store.commit('saveRecords')
+    },
+    saveRecords(state) {
+      window.localStorage.setItem('recordList', JSON.stringify(state.recordList));
+    },
   }
-})
+});
+
+export default store;
