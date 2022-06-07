e45c261f6c2528861fcb823670cfbb31abb69563
xinlo <1311257306@qq.com>
2022-06-07 16:07:56 +0800

    Money.vue使用vuex

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -19,35 +19,28 @@ import Types from '@/components/Money/Types.vue';
 import FormItem from '@/components/Money/FormItem.vue';
 import Tags from '@/components/Money/Tags.vue';
 import {Component} from 'vue-property-decorator';
-import store from '@/store/index2';
-
-
-// const version = window.localStorage.getItem('version') || '0';
-// const recordList: Record[] = JSON.parse(window.localStorage.getItem('recordList') || '[]');
-// if (version === '1.0.0') {
-//   //数据库升级，数据迁移
-//   recordList.forEach(record => {
-//     record.createdAt = new Date(2020, 0, 1);
-//   });
-//   //保存数组
-//   window.localStorage.setItem('recordList', JSON.stringify(recordList));
-// }
-// window.localStorage.setItem('version', '1.0.1');
-
 
 @Component({
   components: {Tags, FormItem, Types, NumberPad},
+  computed:{
+    recordList(){
+      return this.$store.state.recordList;
+    }
+  }
 })
 export default class Money extends Vue {
-  recordList = store.recordList;
-  record: RecordItem = {tags: [], notes: '', type: '-', amount: 0};
+  record: RecordItem =
+      {tags: [], notes: '', type: '-', amount: 0};
+  created(){
+    this.$store.commit('fetchRecords')
+  }
 
   onUpdateNotes(value: string) {
     this.record.notes = value;
   };
 
   saveRecord() {
-    store.createRecord(this.record);
+    this.$store.commit('createRecord',this.record);
   }
 
 
@@ -74,3 +67,14 @@ export default class Money extends Vue {
 </style>
 
 
+// const version = window.localStorage.getItem('version') || '0';
+// const recordList: Record[] = JSON.parse(window.localStorage.getItem('recordList') || '[]');
+// if (version === '1.0.0') {
+//   //数据库升级，数据迁移
+//   recordList.forEach(record => {
+//     record.createdAt = new Date(2020, 0, 1);
+//   });
+//   //保存数组
+//   window.localStorage.setItem('recordList', JSON.stringify(recordList));
+// }
+// window.localStorage.setItem('version', '1.0.1');
